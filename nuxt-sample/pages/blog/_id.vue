6320664b0b08d9d7b8e9dffe2bf821a4093b2983
Ryomanba <datemanba3@gmail.com>
2022-07-01 00:03:55 +0900

    element-ui ダイアログ作成

---
+++ nuxt-sample/pages/blog/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    <h2>blog detail</h2>
+    <p>これは、{{ $route.params.id }} のエントリーです</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // パラメータが全て数字であるか
+    validate({ params }) {
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
